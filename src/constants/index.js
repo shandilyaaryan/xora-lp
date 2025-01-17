67525501793a12c0ea2fa622a040bc9ac948faf1
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-17 22:32:05 +0530

    Moved features to constants

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+    {
+        id: '0',
+        icon: '/images/feature-1.png',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 1',
+        text: 'TEXT 1',
+        button: {
+            icon:'/images/magictouch.svg',
+            title: 'Watch the Demo'
+        }
+    },
+    {
+        id: '1',
+        icon: '/images/feature-2.png',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 2',
+        text: 'TEXT 2',
+        button: {
+            icon:'/images/magictouch.svg',
+            title: 'Watch the Demo'
+        }
+    }
+]
---
+++ src/sections/Features.jsx
@@ -0,0 +1,17 @@
+import { Element } from "react-scroll";
+
+const Features = () => {
+    return ( 
+        <section>
+             <Element name="features">
+                <div className="container">
+                    <div className="relative flex">
+                        
+                    </div>
+                </div>
+             </Element>
+        </section>
+     );
+}
+ 
+export default Features;
