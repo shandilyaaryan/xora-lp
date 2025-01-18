147d0d697d1d32f8acde2d23f52845d298cd61a0
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-18 09:10:50 +0530

    UTH changes

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,4 +1,5 @@
 import { Element } from "react-scroll";
+import Button from "../components/Button";
 import { features } from "../constants";
 
 const Features = () => {
@@ -18,8 +19,14 @@ const Features = () => {
 
                                 <p className="caption mb-5 max-md:mb-6">{caption}</p>
                                 <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">{title}</h2>
+                                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                                <Button icon={button.icon}>{button.title}</Button>
                             </div>
                         ))}
+
+                        <ul className="relative flex justify-around flex-grow px-[5%] border-s3 rounded-7xl max-md:hidden">
+                            
+                        </ul>
                     </div>
                 </div>
              </Element>
