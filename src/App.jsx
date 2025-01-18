5ad540a48a3e32f4040aca7c380c8f6e4c4fe4b5
shandilyaaryan <aryanshandilya10@gmail.com>
2025-01-18 12:04:46 +0530

    Completed Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Download from "./sections/Download";
+import Faq from "./sections/Faq";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonial";
 
 const App = () => {
   return ( 
@@ -10,6 +14,10 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
    );
 }
